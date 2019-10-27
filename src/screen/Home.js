68dba536b968c7ac9878c41397bcbfec199905ca
Melroune <kerkeb@gmail.com>
2019-10-27 02:23:44 +0100

    add redux in Home

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,36 +1,56 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import axios from "axios"
-// import houses from "../assets/json/houses.json"
+// import axios from "axios"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import allTheActions from "../actions"
 import "./Home.css"
 
 class Home extends React.Component {
   state = {
     houses: []
   }
-  getData = async () => {
-    const res = await axios.get(`https://harrypotterapi20.herokuapp.com/houses`)
-    await this.setState({ houses: res.data })
+  getData = () => {
+    this.props.actions.characters.getCharacters(
+      `https://harrypotterapi20.herokuapp.com/houses`
+    )
   }
   componentDidMount() {
     this.getData()
   }
   render() {
-    const { houses } = this.state
+    console.log(this.props.housesState)
+    // const { houses } = this.props
     return (
       <div>
         <div>
-          {houses.map((elem, index) => {
-            return (
-              <Link key={index} to={`/house/${elem.house}`}>
-                <img className="HomeLogo" src={elem.img} alt="poudlard" />
-              </Link>
-            )
-          })}
+          {this.props.loadingState ? (
+            this.props.housesState.map((elem, index) => {
+              return (
+                <Link key={index} to={`/house/${elem.house}`}>
+                  <img className="HomeLogo" src={elem.img} alt="poudlard" />
+                </Link>
+              )
+            })
+          ) : (
+            <div> ...Loading </div>
+          )}
         </div>
       </div>
     )
   }
 }
+const mapStateToProps = state => ({
+  housesState: state.charactersReducer.result,
+  loadingState: state.charactersReducer.loading
+})
+const mapDispatchToProps = () => dispatch => ({
+  actions: {
+    characters: bindActionCreators(allTheActions.characters, dispatch)
+  }
+})
 
-export default Home
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
